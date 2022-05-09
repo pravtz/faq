9f35f7471d47b0ae16ceffa10b03f96dac4ad309
pravtz <ederson.ufrgs@gmail.com>
2022-05-09 14:53:38 -0300

    modify button screenshot app

--- client/src/components/WidgetForm/ScreemShotButton.tsx
+++ client/src/components/WidgetForm/ScreemShotButton.tsx
@@ -1,11 +1,39 @@
-import { Camera } from "phosphor-react";
+import { Camera, Trash } from "phosphor-react";
 import html2canvas from 'html2canvas'
+import { useState } from "react";
+import { Loading } from "./Loading";
 
+interface ScreemShotButtonProps {
+  screenshot: string | null
+  onScreenshotTook: (screenshot: string | null) => void
+}
+export function ScreemShotButton({screenshot, onScreenshotTook}:ScreemShotButtonProps ) {
+  const [isTakingScreenShot, setIsTakingScreenShot] = useState(false)
 
-export function ScreemShotButton() {
   async function handleTakeScreenshot(){
+    setIsTakingScreenShot(true)
     const canvas = await html2canvas(document.querySelector('html')!)
     const base64image = canvas.toDataURL('image/png')
+
+    onScreenshotTook(base64image)
+    setIsTakingScreenShot(false)
+  }
+
+  if(screenshot){
+    return (
+      <button
+        type="button"
+        className="p-1 w-10 rounded-md border-transparent flex justify-end items-end text-zinc-400 hover:text-zinc-100 transition-colors"
+        onClick={()=>onScreenshotTook(null) }
+        style={{
+          backgroundImage:`url(${screenshot})`,
+          backgroundPosition: 'right bottom',
+          backgroundSize:180
+        }}
+      >
+        <Trash weight="fill"/>
+      </button>
+    )
   }
   return (
     <button
@@ -13,7 +41,8 @@ export function ScreemShotButton() {
       onClick={handleTakeScreenshot}
       className="p-2 bg-zinc-800 rounded-md border-transparent hover:bg-zinc-700 transition-color focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500"
     >
-      <Camera className="w-6 h-6" />
+      { isTakingScreenShot ? <Loading /> : <Camera className="w-6 h-6" /> }
+      
     </button>
   )
 } 
